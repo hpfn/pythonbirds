7cdb515515f3ea92df31f6b004a1263ef9981062
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-02-22 15:44:21 -0300

    Fixed renzo.__dict__, attr e métodos da instancia e da classe com comentários

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    # da classe
+    olhos = 2
+
+    # da instancia
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -6,6 +10,14 @@ class Pessoa:
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_attr_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 
 if __name__ == '__main__':
@@ -20,10 +32,21 @@ if __name__ == '__main__':
     for filho in luciano.filhos:
         print(filho.nome)
         print(filho.idade)
+    # da instância
     luciano.sobrenome = 'Ramalho'
     print(luciano.sobrenome)
     del luciano.sobrenome
+    # __dict__ - da instância
     print(luciano.__dict__)
-    print(renzo.__dict___)
+    print(renzo.__dict__)
+    print(f'antes da mudança luciano.olhos - {luciano.olhos}')
+    # da classe - vai todas as instancias
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_attr_de_classe(), luciano.nome_e_attr_de_classe())
 
 
