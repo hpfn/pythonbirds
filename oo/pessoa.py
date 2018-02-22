437fd6b28afc45dc188a3bf18f7555a434323091
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-02-22 14:53:52 -0300

    Criado e removido attr dinâmico de objs do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,9 +17,13 @@ if __name__ == '__main__':
     print(luciano.cumprimentar())
     print(luciano.nome)
     print(luciano.idade)
-    # print(luciano.filhos)
     for filho in luciano.filhos:
         print(filho.nome)
         print(filho.idade)
+    luciano.sobrenome = 'Ramalho'
+    print(luciano.sobrenome)
+    del luciano.sobrenome
+    print(luciano.__dict__)
+    print(renzo.__dict___)
 
 
