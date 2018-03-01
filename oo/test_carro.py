b40f49a03bbedc0e94c7ebdd915eeec100f13e32
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-03-01 13:32:17 -0300

    Testes para a classe Direcao completo.

--- oo/test_carro.py
+++ oo/test_carro.py
@@ -1,20 +1,55 @@
 from unittest import TestCase
-from oo.carro import Motor
+from oo.carro import Motor, Direcao, Carro
 
 
 class CarroTestCase(TestCase):
+    def setUp(self):
+        self.motor = Motor()
+        self.direcao = Direcao()
+
     def test_velocidade_inicial(self):
-        motor = Motor()
-        self.assertEqual(0, motor.velocidade)
+        self.assertEqual(0, self.motor.velocidade)
 
     def test_acelerar(self):
-        motor = Motor()
-        motor.acelerar()
-        self.assertEqual(1, motor.velocidade)
+        self.motor.acelerar()
+        self.assertEqual(1, self.motor.velocidade)
 
     def test_frear(self):
-        motor = Motor()
-        motor.acelerar()
-        motor.frear()
-        self.assertEqual(0, motor.velocidade)
+        self.motor.acelerar()
+        self.motor.frear()
+        self.assertEqual(0, self.motor.velocidade)
+
+    def test_direcao_inicial(self):
+        self.assertEqual('Norte', self.direcao.valor)
+
+    def test_direcao_direita(self):
+        rumo = list(self.direcao.rotacao_a_direita.values())
+
+        with self.subTest():
+            for i in rumo:
+                self.direcao.girar_a_direita()
+                self.assertEqual(i, self.direcao.valor)
+
+    def test_direcao_esquerda(self):
+        rumo = list(self.direcao.rotacao_a_esquerda.values())
+
+        with self.subTest():
+            for i in rumo:
+                self.direcao.girar_a_esquerda()
+                self.assertEqual(i, self.direcao.valor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
